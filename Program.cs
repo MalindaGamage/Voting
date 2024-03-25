b08e2f915a614158be7aba2f0c539e2e297563c4
DELL <DELL@MPKG-PC>
2024-03-25 23:39:14 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Voting
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("How old are you?");
+            int input = int.Parse(Console.ReadLine());
+
+            int Presidents = CalculatePresidents(input);
+            Console.WriteLine($"You have voted for {Presidents} Presidents!");
+            Console.ReadKey();
+        }
+
+        public static int CalculatePresidents(int age)
+        {
+            int eligibleYears = age - 18;
+            int Presidents = 0;
+
+            if (eligibleYears <= 0)
+            {
+                Console.WriteLine("Look like You are not old enough to vote!");
+                return 0;
+            }
+
+            for (int i = 0; i < eligibleYears; i++)
+            {
+                if (i % 4 == 0)
+                {
+                    Presidents++;
+                }
+            }
+            return Presidents;
+        }
+    }
+}
